a1b80e6dc028f4e691eb9f8ce20b313ebd169517
tewodrosamare <teddyamare1621@gmail.com>
2026-03-31 18:05:18 +0300

    Initial commit for partner branch

--- views/layout/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2026 <a href="">Warka Hub</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.0
-    </div>
-  </footer>
-  
-  
